2c285a38cff8ab4068fd9a5adb5259e89e49479d
wilbertrodriguez <wilbertrodriguez@csu.fullerton.edu>
2019-08-31 13:32:45 -0700

    My star

--- prob01/main.cpp
+++ prob01/main.cpp
@@ -1,10 +1,11 @@
-// Name: 
+// Name:
 // This program displays a diamond pattern to the screen.
 
 #include <iostream>
 
 int main()
 {
+std :: cout << "   *   \n  ***   \n ***** \n*******\n ***** \n  *** \n   *   ";
 
   return 0;
 }
