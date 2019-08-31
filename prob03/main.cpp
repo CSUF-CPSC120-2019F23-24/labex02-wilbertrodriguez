7d3fb9fe96c6bebfb8233899999cafab59e89ea0
wilbertrodriguez <wilbertrodriguez@csu.fullerton.edu>
2019-08-31 14:21:34 -0700

    Total Bill includes tip and taxes

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,2 +1,20 @@
-// Name: 
+// Name:Wilbert Rodriguez
 // This program calculates the tax and tip on a restaurant bill.
+
+#include <iostream>
+
+int main()
+{
+  double mealcost,waitertip,taxes;
+
+  std::cout << "Welcome to the Restaurant Bill calculater!\n";
+  std::cout << "What is the total meal cost? ";
+  std::cin >> mealcost;
+  taxes=mealcost*.0775;
+  waitertip=mealcost*.20;
+  double totalcost=mealcost+taxes+waitertip;
+  std::cout << "Tax:        $" << taxes << "\n";
+  std::cout << "Tip:        $" << waitertip << "\n";
+  std::cout << "Total Bill: $" << totalcost << "\n";
+
+}
