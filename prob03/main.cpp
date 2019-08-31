e9adb413e68145fe64f5a04e5efbbe25e137e61c
wilbertrodriguez <54484240+wilbertrodriguez@users.noreply.github.com>
2019-08-31 14:36:40 -0700

    Update main.cpp

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -5,6 +5,7 @@
 
 int main()
 {
+  //Added double variables so decimals can be inserted.
   double mealcost,waitertip,taxes;
 
   std::cout << "Welcome to the Restaurant Bill calculator!\n";
@@ -12,7 +13,9 @@ int main()
   std::cin >> mealcost;
   taxes=mealcost*.0775;
   waitertip=mealcost*.20;
+  //The total cost was added later so no unexpected prices appeared.
   double totalcost=mealcost+taxes+waitertip;
+  //Added \n at the end so the code would move on to the next line.
   std::cout << "Tax:        $" << taxes << "\n";
   std::cout << "Tip:        $" << waitertip << "\n";
   std::cout << "Total Bill: $" << totalcost << "\n";
