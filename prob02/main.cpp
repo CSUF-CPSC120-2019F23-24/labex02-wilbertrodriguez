3209841e69cf752db1123ab2af1f257fa6be5d27
wilbertrodriguez <wilbertrodriguez@csu.fullerton.edu>
2019-08-31 13:55:32 -0700

    No errors

--- prob02/main.cpp
+++ prob02/main.cpp
@@ -1,4 +1,4 @@
-// Name: 
+// Name:
 // This program calculates the area of a room.
 
 #include <iostream>
@@ -9,16 +9,18 @@ int main()
   int area;
 
   // Get the length and width of the room.
-  std::cout << "What is the room's "width" in feet? ";
+  std :: cout << "What is the room's \"width\" in feet? ";
   std::cin >> width;
-  std::cout << "What is the room's "length" in feet? ";
-  std::cin >> area;
+  std::cout << "What is the room's \"length\" in feet? ";
+  std::cin >> length;
 
   // Calculate the area of the room.
-  area = length / width;
+  area = length * width;
 
   // Display the area of the room.
-  std::cout << "The \"area\" of the room is 204.7 square feet./n";
+  std::cout << "The \"area\" of the room is ";
+  std::cout<< area;
+  std::cout << " square feet./n";
 
   return 0;
 }
